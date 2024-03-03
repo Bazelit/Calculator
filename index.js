cc2280f89739a2b8e8665a107a7cea5ac9e8af77
Bazelit <davidgumashyan777@gmail.com>
2024-03-03 13:30:52 +0300

    readme.md

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-// npx rollup -p @rollup/plugin-node-resolve index.js -o bundle.js
-
-// import "@material/web/button/filled-tonal-button.js";
-// import "@material/web/button/outlined-button.js";
-// import "@material/web/checkbox/checkbox.js";
-// import "@material/web/FAB/FAB.js";
-// import "@material/web/switch/switch.js";
-// import "@material/web/textfield/filled-text-field.js";
-// import "@material/web/textfield/outlined-text-field.js";
-
 const themeSwitch = document.querySelector(".theme__switch");
 const wrapper = document.querySelector(".wrapper");
+const display = document.querySelector(".display");
 
-themeSwitch.addEventListener("click", () => {
-  wrapper.classList.toggle("active");
-});
+const appendToDisplay = (value) => (display.value += value);
+const clearDisplay = () => (display.value = "");
+const backspace = () => (display.value = display.value.slice(0, -1));
+const power = () => (display.value = Math.pow(parseFloat(display.value), 2));
+
+themeSwitch.addEventListener("click", () => wrapper.classList.toggle("active"));
+display.addEventListener("keydown", (event) => event.key === "Enter" ?  calculate() : false);
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
