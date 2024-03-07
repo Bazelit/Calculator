17ec60689d566d1e2ca52cfd4dcb90bb6fd3f28c
Bazelit <davidgumashyan777@gmail.com>
2024-03-07 14:15:12 +0300

    readme.md

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const themeSwitch = document.querySelector(".theme__switch");
 const wrapper = document.querySelector(".wrapper");
 const display = document.querySelector(".display");
-const body  = document.querySelector("body")
+const body = document.querySelector("body");
 
 const appendToDisplay = (value) => (display.value += value);
 const clearDisplay = () => (display.value = "");
 const backspace = () => (display.value = display.value.slice(0, -1));
 const power = () => (display.value = Math.pow(parseFloat(display.value), 2));
 
-
 themeSwitch.addEventListener("click", () => body.classList.toggle("active"));
-display.addEventListener("keydown", (event) => event.key === "Enter" ?  calculate() : false);
+display.addEventListener("keydown", (event) =>
+  event.key === "Enter" ? calculate() : false
+);
 
 function calculate() {
   try {
