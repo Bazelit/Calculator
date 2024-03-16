0354bfae0ae5088934856a309ebac565f4e52a7f
Bazelit <davidgumashyan777@gmail.com>
2024-03-16 16:45:31 +0300

    ​⠀​⠀​

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+import "@material/web/button/filled-tonal-button.js";
+import "@material/web/button/elevated-button.js";
+import "@material/web/button/filled-button.js";
+import "@material/web/switch/switch.js";
+import "@material/web/textfield/outlined-text-field.js";
+
 const themeSwitch = document.querySelector(".theme__switch");
-const wrapper = document.querySelector(".wrapper");
 const display = document.querySelector(".display");
 const body = document.querySelector("body");
 
